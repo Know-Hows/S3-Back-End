f6dd56243579b059df6c24fc78ca6a8dcb56d883
Jimmy <jimmysch123@gmail.com>
2023-09-13 15:13:50 +0200

    Database connection

--- KnowHows Back-End/KnowHows Back-End/Program.cs
+++ KnowHows Back-End/KnowHows Back-End/Program.cs
@@ -1,3 +1,6 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,6 +19,23 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+const string connectionUri = "mongodb+srv://435518:<password>@knowhowscluster.iwmreky.mongodb.net/?retryWrites=true&w=majority";
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+// Set the ServerApi field of the settings object to Stable API version 1
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+// Create a new client and connect to the server
+var client = new MongoClient(settings);
+// Send a ping to confirm a successful connection
+try
+{
+    var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+    System.Diagnostics.Debug.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+}
+catch (Exception ex)
+{
+    System.Diagnostics.Debug.WriteLine(ex);
+    System.Diagnostics.Debug.WriteLine("You fucking suck");
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
