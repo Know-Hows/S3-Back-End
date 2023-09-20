deabfd53073f7509466f5b5d55e8e847cdbab665
Espilonius <58418773+Espilonius@users.noreply.github.com>
2023-09-20 10:24:19 +0200

    DBSettings

--- KnowHows Back-End/KnowHows Back-End/Models/KnowHowsDatabaseSettings.cs
+++ KnowHows Back-End/KnowHows Back-End/Models/KnowHowsDatabaseSettings.cs
@@ -2,8 +2,8 @@
 {
     public class KnowHowsDatabaseSettings
     {
-        public string ConnectionString { get; set; } = string.Empty;
-        public string DatabaseName { get; set; } = string.Empty;
-        public string ArticleCollectionName { get; set; } = string.Empty;
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string ArticleCollectionName { get; set; } = null!;
     }
 }
