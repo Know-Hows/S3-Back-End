b3be470c3bd5c39abab009612281689551320433
Espilonius <58418773+Espilonius@users.noreply.github.com>
2023-11-01 15:18:05 +0100

    Added API-key middleware authorization

--- KnowHows Back-End/Program.cs
+++ KnowHows Back-End/Program.cs
@@ -3,6 +3,8 @@ using MongoDB.Driver;
 using KnowHows_Back_End.Services;
 using KnowHows_Back_End.Models;
 using KnowHows_Back_End.Interfaces;
+using KnowHows_Back_End.Authentication;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,11 +16,40 @@ builder.Services.AddSingleton<ArticleService>();
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme()
+    {
+        Description = "Api Key to secure the API",
+        Type = SecuritySchemeType.ApiKey,
+        Name = AuthConfig.ApiKeyHeader,
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+
+    var scheme = new OpenApiSecurityScheme()
+    {
+        Reference = new OpenApiReference()
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+
+    var requirement = new OpenApiSecurityRequirement()
+    {
+        { scheme, new List<string>() }
+    };
+
+    x.AddSecurityRequirement(requirement);
+});
 
 builder.Services.AddScoped<IArticleService, ArticleService>();
 
 builder.Services.AddCors();
+
+builder.Services.AddScoped<ApiKeyAuthenticationFilter>();
 
 var app = builder.Build();
 
@@ -34,6 +65,8 @@ app.UseCors(x => x
                 .AllowAnyHeader());
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<ApiKeyMiddleware>();
 
 app.UseAuthorization();
 
